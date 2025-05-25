ba89986bfa1fb5550d91644a6903bef7d3082b6c
EDWINKOHWEISHAN <147369431+EDWINKOHWEISHAN@users.noreply.github.com>
2025-05-26 07:24:21 +0800

    Write Messages to Firestore

--- step_02/lib/home_page.dart
+++ step_02/lib/home_page.dart
@@ -1,11 +1,13 @@
-import 'package:firebase_auth/firebase_auth.dart' // new
-    hide EmailAuthProvider, PhoneAuthProvider; // new
-import 'package:flutter/material.dart'; // new
-import 'package:provider/provider.dart'; // new
+import 'package:firebase_auth/firebase_auth.dart'
+    hide EmailAuthProvider, PhoneAuthProvider;
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import 'app_state.dart'; // new
-import 'src/authentication.dart'; // new
+import 'app_state.dart';
+import 'guest_book.dart';
+import 'src/authentication.dart';
 import 'src/widgets.dart';
+import 'guest_book.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -40,6 +42,20 @@ class HomePage extends StatelessWidget {
           const Header("What we'll be doing"),
           const Paragraph(
             'Join us for a day full of Firebase Workshops and Pizza!',
+          ),
+          Consumer<ApplicationState>(
+            builder: (context, appState, _) => Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (appState.loggedIn) ...[
+                  const Header('Discussion'),
+                  GuestBook(
+                    addMessage: (message) =>
+                        appState.addMessageToGuestBook(message),
+                  ),
+                ],
+              ],
+            ),
           ),
         ],
       ),
